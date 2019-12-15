8f7208ccb3470e2931896838a9f8ccf96931bf60
Boris Resnick <boris@resnick.ru>
2019-12-15 10:08:12 +0300

    Day 12

--- day_12.jl
+++ day_12.jl
@@ -40,6 +40,27 @@ function iterate!(moons, num)
     return moons
 end
 
+function iterate2begin(moons, coord)
+    cslice = ms -> map(ms) do m
+        m[coord]
+    end    
+    startmoons = cslice(deepcopy(moons))
+    step = 0
+    while true
+        makestep!(moons)
+        step += 1
+        if cslice(moons) == startmoons
+            return step
+        end
+    end
+end
+
+function findback(moons)
+    map(1:3) do coord
+        iterate2begin(moons, coord)
+    end |> lcm
+end
+
 function energy(moons)
     map(moons) do moon
         (sc, sv) = (0.0, 0.0)
@@ -56,6 +77,15 @@ end
 <x=2, y=-10, z=-7>
 <x=4, y=-8, z=8>
 <x=3, y=5, z=-1>
-""" |> load |> m -> iterate!(m, 10) |> energy |> println
+""" |> load |> m -> iterate!(m, 10) |> energy |> (x -> x == 179) |> println
 
 read("input_d12.txt", String) |> load |> m -> iterate!(m, 1000) |> energy |> println
+
+"""
+<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>
+""" |> load |> findback |> (x -> x == 4686774924)|> println
+
+read("input_d12.txt", String) |> load |> findback |> println
